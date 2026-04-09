0ae25ae793fdf361a6d61a037ed57a046c011b8c
Faizan shaikh <faizan.shaikh@regur.net>
2026-04-09 12:30:12 +0530

    Converted project to New Architecture Full API + JS

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-  <div class="container justify-content-center d-flex">
-    <a href="/conduit/" class="logo-font">conduit</a>
-    <span class="attribution">
-      An interactive learning project. Code &amp; design licensed under MIT.
-    </span>
-  </div>
-</footer>
