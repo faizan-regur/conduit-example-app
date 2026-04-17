0f8830b1f21a02ca16d19a129c95999ce4a3f057
Faizan shaikh <faizan.shaikh@regur.net>
2026-04-17 19:13:03 +0530

    Updated handle put function (not complete)

--- backend/includes/handlePut.php
+++ backend/includes/handlePut.php
@@ -3,17 +3,26 @@
 function handlePut($conn, $data, $request){
     if($request === '/conduit/api/user'){
         // Validation
-        if(empty($data['user']['bio']) && empty($data['user']['image']) && empty($data['user']['email']) && empty($data['user']['password']) && empty($data['user']['username'])){
+        if( empty($data['user']['email']) && empty($data['user']['username'])){
             http_response_code(422);
             echo json_encode(["error" => "Please enter value to update"]);
             exit;
         }
-
-        // $username = $conn->real_escape_string($data['user']['username']);
-        // $email = $conn->real_escape_string($data['user']['email']);
+        
+        $username = $conn->real_escape_string($data['user']['username']);
+        $email = $conn->real_escape_string($data['user']['email']);
         // $password = password_hash($data['user']['password'], PASSWORD_BCRYPT);
         // $image = $conn->real_escape_string($data['user']['image']);
         // $bio = $conn->real_escape_string($data['user']['bio']);
 
+        
+        $query = "UPDATE users SET username=$username WHERE email = $email";
+
+        if ($conn->query($query)) {
+            echo json_encode(['message' => 'User updated successfully']);
+        } else {
+            http_response_code(500);
+            echo json_encode(["error" => "Failed to update user"]);
+        }
     }
 }
